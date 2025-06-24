239445874cb6cdeabb259abb3a51f3e75ab2e9be
Ancarratt <carrattantoine@gmail.com>
2025-06-24 09:35:47 +0200

    Update send_message.py

--- send_message.py
+++ send_message.py
@@ -1,6 +1,7 @@
+import os
 import requests
 
-webhook_url = "https://discord.com/api/webhooks/TON_WEBHOOK_ID/TON_TOKEN"
+webhook_url = os.environ["DISCORD_WEBHOOK"]
 message = {
     "content": "N'oubliez pas de faire votre tentative gratuite avant 10 heure / 22 heure ! Et on recharge son boost d'entrainement ! @Sportif"
 }
