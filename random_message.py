5a1e1cbb0464db31326182939cfa6a2c298c6479
Ancarratt <carrattantoine@gmail.com>
2026-03-16 13:53:13 +0100

    Update random_message.py

--- random_message.py
+++ random_message.py
@@ -7,19 +7,21 @@ import requests
 WEBHOOK_URL = os.environ["DISCORD_WEBHOOK"]
 
 # Délai aléatoire jusqu'à 30 minutes
-delay = random.randint(0, 10)
+delay = random.randint(0, 100)
 print(f"[INFO] Attente de {delay} secondes avant de décider d’envoyer un message.")
 time.sleep(delay)
 
 # 40 % de chances d'envoyer un message
 if random.random() < 0.9:
     messages = [
-        "<@1014296748623155200> t’es ma petite salope … envoie un BC si t’es un homme!",
-        "<@1014296748623155200> t’es ma petite salope … envoie un BC si t’es un",
-        "<@1014296748623155200> t’es ma petite salope … envoie un BC si t’es",
-        "<@1014296748623155200> t’es ma petite salope … envoie un BC si",
-        "<@1014296748623155200> t’es ma petite salope … envoie un BC",
-        "<@1014296748623155200> t’es ma petite salope … envoie un"
+        "<@334048588248252426> t’es ma petite salope … envoie un BC si t’es un homme!",
+        "<@308656553907585025> t’es ma petite salope … envoie un BC si t’es un homme!",
+        "<@511005405795844117> t’es ma petite salope … envoie un BC si t’es un homme!",
+        "<@416707605390426124> t’es ma petite salope … envoie un BC si t’es un homme!",
+        "<@367632872234024961> t’es ma petite salope … envoie un BC si t’es un homme!",
+        "<@195994843556741120> t’es ma petite salope … envoie un BC si t’es un homme!",
+        "<@199220568841519115> t’es ma petite salope … envoie un BC si t’es un homme!",
+        "<@372826301259710465> t’es ma petite salope … envoie un BC si t’es un homme!"
     ]
     message = f" {random.choice(messages)} "
     payload = {"content": message}
