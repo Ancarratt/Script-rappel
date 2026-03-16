ecdea9a74812dfcf1e40c03630e2ee224eadafda
Ancarratt <carrattantoine@gmail.com>
2026-03-16 13:41:41 +0100

    Update random_message.py

--- random_message.py
+++ random_message.py
@@ -14,12 +14,12 @@ time.sleep(delay)
 # 40 % de chances d'envoyer un message
 if random.random() < 0.9:
     messages = [
-        "<@1352573963343237120> t’es ma petite salope … envoie un BC si t’es un homme!",
-        "<@1352573963343237120> t’es ma petite salope … envoie un BC si t’es un",
-        "<@1352573963343237120> t’es ma petite salope … envoie un BC si t’es",
-        "<@1352573963343237120> t’es ma petite salope … envoie un BC si",
-        "<@1352573963343237120> t’es ma petite salope … envoie un BC",
-        "<@1352573963343237120> t’es ma petite salope … envoie un"
+        "<@1014296748623155200> t’es ma petite salope … envoie un BC si t’es un homme!",
+        "<@1014296748623155200> t’es ma petite salope … envoie un BC si t’es un",
+        "<@1014296748623155200> t’es ma petite salope … envoie un BC si t’es",
+        "<@1014296748623155200> t’es ma petite salope … envoie un BC si",
+        "<@1014296748623155200> t’es ma petite salope … envoie un BC",
+        "<@1014296748623155200> t’es ma petite salope … envoie un"
     ]
     message = f" {random.choice(messages)} "
     payload = {"content": message}
